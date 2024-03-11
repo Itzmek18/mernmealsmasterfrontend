39d989e8d57ae011c4bf1d6bdf2d3d23bfd99e6b
itzmek18 <keerthanakalidoss2004@gmail.com>
2024-03-11 05:28:49 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://png.pngtree.com/png-vector/20230228/ourmid/pngtree-male-chef-in-restaurant-presents-fresh-vegetables-for-saladmaking-on-plate-vector-illustration-vector-png-image_49891494.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
